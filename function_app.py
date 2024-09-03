58470c0fa411ee8d8f2214a0aeacc9a930042c9e
tanabeJJ <tanabe@japannext.net>
2024-09-03 19:44:16 +0900

    test

--- function_app.py
+++ function_app.py
@@ -63,7 +63,7 @@ def timer_trigger1(myTimer: func.TimerRequest) -> None:
 @app.timer_trigger(schedule="* * * * * *", arg_name="myTimer", run_on_startup=True,
               use_monitor=False) 
 def timer_trigger3(myTimer: func.TimerRequest) -> None:
-    
+    print("タイマートリガー開始")
     if myTimer.past_due:
         logging.info('The timer is past due!')
 
