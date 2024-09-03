c72afbda1dcad702d0574009af4149427a34a8ee
tanabeJJ <tanabe@japannext.net>
2024-09-03 19:11:01 +0900

    test

--- function_app.py
+++ function_app.py
@@ -55,3 +55,4 @@ def timer_trigger1(myTimer: func.TimerRequest) -> None:
     if myTimer.past_due:
         logging.info('The timer is past due!')
     logging.info('Python timer trigger function executed.')
+    return "abc"
